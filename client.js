a2cf5dcb9a9bb25dfc6034b1777e58dfed7954b4
Zach Carter <thezachcarter@gmail.com>
2022-03-04 15:46:27 -0600

    completed addEmployee function

--- client.js
+++ client.js
@@ -3,21 +3,25 @@ console.log('JS ready');
 $(readyNow);
 function readyNow(){
     console.log('JQ ready')
+    $('#submitBtn').on('click', addEmployee);
 }
 
 let employeeArray = [];
 
 //function to create a new object from info in input field
 //and push to a new array
-function addEmployee {
+function addEmployee () {
     console.log('in addEmployee function');
     
     let employee = {
-        firstName : ;
-        lastName : ;
-        ID : ;
-        Title : ;
-        Annual Salary : ;
+        firstName : $('#firstName').val(),
+        lastName : $('#lastName').val(),
+        ID : $('#ID').val(),
+        title : $('#title').val(),
+        annuaSalary : $('#annualSalary').val()
     }//end employeeObject
     employeeArray.push(employee);
+    console.log(employee);
+    console.log(employeeArray);
 }//end addEmployee function
+
