191ba784c2fef2b3bf99fb67e506a79a868de437
Zach Carter <thezachcarter@gmail.com>
2022-03-04 15:28:33 -0600

    added function to creat employee object

--- client.js
+++ client.js
@@ -4,3 +4,20 @@ $(readyNow);
 function readyNow(){
     console.log('JQ ready')
 }
+
+let employeeArray = [];
+
+//function to create a new object from info in input field
+//and push to a new array
+function addEmployee {
+    console.log('in addEmployee function');
+    
+    let employee = {
+        firstName : ;
+        lastName : ;
+        ID : ;
+        Title : ;
+        Annual Salary : ;
+    }//end employeeObject
+    employeeArray.push(employee);
+}//end addEmployee function
